9db1aec3d11218de60a0c30838e35090026ba71d
Pouya Mazloumpour <mazloumpour.pouya@gmail.com>
2023-12-04 11:03:25 -0800

    initial commit

---
+++ app/src/main/java/com/pouyaa/takehomegithubapi/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.pouyaa.takehomegithubapi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pouyaa.takehomegithubapi.ui.theme.TakeHomeGithubAPITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TakeHomeGithubAPITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TakeHomeGithubAPITheme {
+        Greeting("Android")
+    }
+}
